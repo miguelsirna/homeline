b1e2073a8851f876138fd37989a2342b6fe0984e
miguelsirna <contacto@miguelsirna.com>
2014-09-15 02:39:54 -0500

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homeline::Application.config.secret_token = '1c1a416c34292879c164df201aeb16b9f56de61ea42e8f32fd48c92e6bb3e8cb6e544e7bb36f49c494b9544e9882cc9c36adc6ca8a77f8ec0e7fc63e3ffa4e55'
